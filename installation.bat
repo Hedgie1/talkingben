7fb8f4b6300df71e9083f6e29fcdd1c667068221
Hedgie <64465772+Hedgie1@users.noreply.github.com>
2022-03-23 21:19:32 +0100

    Create installation.bat

---
+++ installation.bat
@@ -0,0 +1,23 @@
+@echo off
+set NULL_VAL=null
+set NODE_VER=%NULL_VAL%
+set NODE_EXEC=node-v16.14.2-x86.msi
+
+node -v >.tmp_nodever
+set /p NODE_VER=<.tmp_nodever
+del .tmp_nodever
+
+IF "%NODE_VER%"=="%NULL_VAL%" (
+	echo.
+	echo Node.js is not installed! Please press a key to download and install it from the website that will open.
+	PAUSE
+	start "" http://nodejs.org/dist/v16.14.2/%NODE_EXEC%
+	echo.
+	echo.
+	echo After you have installed Node.js, press a key to shut down this process. Please restart it again afterwards.
+	PAUSE
+	EXIT
+) ELSE (
+	echo A version of Node.js ^(%NODE_VER%^) is installed. Proceeding...
+	npm install
+)
